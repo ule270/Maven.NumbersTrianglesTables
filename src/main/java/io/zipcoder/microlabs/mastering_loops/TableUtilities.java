103c9792b7be359f9103c346721fde572abc43f2
ule270 <u.le270@gmail.com>
2023-03-05 11:30:46 -0500

    Large table completed.

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -19,10 +19,34 @@ public class TableUtilities {
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String expected = "";
+        for (Integer i = 1; i <= 10; i++) {
+            for (Integer j = 1; j <= 10; j++) {
+                Integer answer = j * i;
+                if (answer > 9) {
+                    String line = " " + answer.toString() + " |";
+                    expected = expected + line;
+                } else {
+                    String line = "  " + answer.toString() + " |";
+                    expected = expected + line;
+                }
+                if (answer > 90) {
+                    String line = "" + answer.toString() + " |";
+                    expected = expected + line;
+                }
+            } expected = expected + "\n";
+        }
+        return expected;
     }
 
     public static String getMultiplicationTable(int tableSize) {
         return null;
+//         if (answer > 99 ) {
+//             String line = "" + answer.toString() + " |";
+//             expected = expected + line;
+//                } else if (answer > 9){
+//             String line = " " + answer.toString() + " |";
+//             expected = expected + line;
+//         } else
     }
 }
