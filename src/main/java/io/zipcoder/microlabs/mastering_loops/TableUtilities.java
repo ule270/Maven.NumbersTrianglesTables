170e3b6dc067a82b68091593fe8cc60d287041ff
ule270 <u.le270@gmail.com>
2023-03-05 00:37:39 -0500

    small mult table done

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,7 +2,20 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+      String expected = "";
+        for (Integer i = 1; i <= 5; i++) {
+            for (Integer j = 1; j <= 5; j++) {
+                Integer answer = j * i;
+                if (answer > 9 ) {
+                    String line = " " + answer.toString() + " |";
+                    expected = expected + line;
+                } else {
+                    String line = "  " + answer.toString() + " |";
+                    expected = expected + line;
+                }
+            } expected = expected + "\n";
+        }
+        return expected;
     }
 
     public static String getLargeMultiplicationTable() {
