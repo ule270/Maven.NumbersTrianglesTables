51dfe34746e370ac200843010a107c7502bef784
ule270 <u.le270@gmail.com>
2023-03-05 11:41:33 -0500

    all tables done.

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,18 +2,19 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-      String expected = "";
+        String expected = "";
         for (Integer i = 1; i <= 5; i++) {
             for (Integer j = 1; j <= 5; j++) {
                 Integer answer = j * i;
-                if (answer > 9 ) {
+                if (answer > 9) {
                     String line = " " + answer.toString() + " |";
                     expected = expected + line;
                 } else {
                     String line = "  " + answer.toString() + " |";
                     expected = expected + line;
                 }
-            } expected = expected + "\n";
+            }
+            expected = expected + "\n";
         }
         return expected;
     }
@@ -23,30 +24,40 @@ public class TableUtilities {
         for (Integer i = 1; i <= 10; i++) {
             for (Integer j = 1; j <= 10; j++) {
                 Integer answer = j * i;
-                if (answer > 9) {
+                if (answer > 90) {
+                    String line = "" + answer.toString() + " |";
+                    expected = expected + line;
+                } else if (answer > 9) {
                     String line = " " + answer.toString() + " |";
                     expected = expected + line;
                 } else {
                     String line = "  " + answer.toString() + " |";
                     expected = expected + line;
                 }
-                if (answer > 90) {
-                    String line = "" + answer.toString() + " |";
-                    expected = expected + line;
-                }
-            } expected = expected + "\n";
+            }
+            expected = expected + "\n";
         }
         return expected;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
-//         if (answer > 99 ) {
-//             String line = "" + answer.toString() + " |";
-//             expected = expected + line;
-//                } else if (answer > 9){
-//             String line = " " + answer.toString() + " |";
-//             expected = expected + line;
-//         } else
+        String expected = "";
+        for (Integer i = 1; i <= 20; i++) {
+            for (Integer j = 1; j <= 20; j++) {
+                Integer answer = j * i;
+                if (answer > 99) {
+                    String line = "" + answer.toString() + " |";
+                    expected = expected + line;
+                } else if (answer > 9) {
+                    String line = " " + answer.toString() + " |";
+                    expected = expected + line;
+                } else {
+                    String line = "  " + answer.toString() + " |";
+                    expected = expected + line;
+                }
+            }
+            expected = expected + "\n";
+        }
+        return expected;
     }
 }
