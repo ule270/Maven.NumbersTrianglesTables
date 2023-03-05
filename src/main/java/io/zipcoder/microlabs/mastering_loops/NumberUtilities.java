f2438f9c75b19cf78ffce3b0f26c74d70e7e88ce
ule270 <u.le270@gmail.com>
2023-03-05 12:39:53 -0500

    squarenumbers done.

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -13,15 +13,27 @@ public class NumberUtilities {
     }
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String expected = "";
+        for (Integer i = start; i <= stop; i++) {
+            if (i%2 == 1){
+                expected = expected + i.toString();
+            }
+        }
+        return expected;
     }
 
-
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String expected = "";
+        for (Integer i = start; i < stop; i++) {
+            if (i%5 == 0) {
+                Integer s = i * i;
+                expected = expected + s.toString();
+            }
+        } return expected;
     }
 
     public static String getRange(int start) {
+
         return null;
     }
 
@@ -36,6 +48,10 @@ public class NumberUtilities {
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+            String expected = "";
+            for (Integer i = start; i <= stop; i++) {
+
+        }
+            return null;
     }
 }
