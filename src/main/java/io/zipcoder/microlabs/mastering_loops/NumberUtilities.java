f6f50793df2bdc428c7ee6e308d529cd1f45d57d
ule270 <u.le270@gmail.com>
2023-03-05 00:00:20 -0500

    I have added changes.

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,9 +3,14 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String expected = "";
+        for (Integer i = start; i <= stop; i++) {
+            if (i%2 == 0){
+               expected = expected + i.toString();
+            }
+        }
+        return expected;
     }
-
 
     public static String getOddNumbers(int start, int stop) {
         return null;
